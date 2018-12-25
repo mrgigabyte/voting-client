fce2b46dbc711e63423bafc761cb920d31a6d9af
exio4 <me@exio4.xyz>
2018-12-25 17:04:47 -0300

    Handle component/render and non Router components gracefully

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -11,9 +11,20 @@ class App extends React.Component {
     render(){
         let children = []
         React.Children.forEach(this.props.children, (child) => {
-            children.push(React.cloneElement(child, {
-                component: Augment(child.props.component)
-            }))
+            if (child.props && child.props.component) {
+                // if it has a component props, we assume it is a router child and thus we need to augment it
+                children.push(React.cloneElement(child, {
+                    component: Augment(child.props.component)
+                }))
+            } else if (child.props && child.props.render) {
+                // render is also a possibility - handle it too
+                children.push(React.cloneElement(child, {
+                    render: Augment(child.props.render)
+                }))
+            } else {
+                // pass-through if is not found to be a router child
+                children.push(child)
+            }
         })
         return children
     }
